98e5b996534d3e0150bac69a9cac2c1c018420e8
Bisoncoder <nehocira@gmail.com>
2025-09-11 16:44:40 -0400

    First upload of repo. If this works first time, I'm doing 5 push-ups.

---
+++ Labs/Lab1_Introduction/src/main/java/com/mycompany/lab1_introduction/Lab1_Introduction.java
@@ -0,0 +1,14 @@
+
+package com.mycompany.lab1_introduction;
+
+public class Lab1_Introduction {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hello. My name is Ari Cohen.");
+        System.out.println("I am majoring in Tri-C's Programming and Development AAB.");
+        System.out.println("My goal for this class is to get my foot in the door of Java.");
+        System.out.println("Post-degree I'd like to get a job in backend dev, but since I have yet to take a front-end class, that might change.");
+        System.out.println("Or I might drop programming as a job altogether and go for welding.");
+    }
+}
